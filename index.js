a099f322b5fbae6832eab6aac87e05b7151fe980
sammylwolfson <samuel.levi.wolfson@gmail.com>
2022-05-08 20:32:56 -0700

    added function to create readme

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+
+
+
+    
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
